0db3c5cf3f13f3b0ceb34ed79e77ccd93898537c
drmyersii <drmyersii@users.noreply.github.com>
2015-01-10 14:47:22 -0500

    Autoloading Controllers, Models, and Routes.

--- tests/server.js
+++ tests/server.js
@@ -3,6 +3,13 @@ var assert = require('assert');
 var express = require('express');
 var app = express();
 
+var load = require('express-load')
+
+load('../models', { verbose: true })
+	.then('../controllers', { verbose: true })
+	.then('../routes')
+	.into(app);
+
 
 // Run basic test on Router to make sure no exceptions are thrown.
 describe('Server', function ()
@@ -11,9 +18,7 @@ describe('Server', function ()
 	{
 		it('Calling Route Method', function ()
 		{
-			var router = require('../router');
-
-			assert.equal(router.Route(app, express), true);
+			assert.equal(app.routes.router.routes.all(app, express), true);
 		});
 	});
 });
